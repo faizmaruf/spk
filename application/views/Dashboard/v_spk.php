72b377442c7c850f828c65b95e65fff1ebe802e6
faizmaruf <faiz.maruf8888@gmail.com>
2022-06-20 14:24:19 +0700

    first commit

--- application/views/Dashboard/v_spk.php
+++ application/views/Dashboard/v_spk.php
@@ -210,6 +210,7 @@
                     <!-- Content Row Tabel Nilai Tiap Alternatif -->
                     <?php $this->load->view('Dashboard/History/v_tabledata',$tabledata); ?>
                     
+                    
                     <!-- Content Row 2. MENORMALISASI TABEL -->
                     <?php $this->load->view('Dashboard/History/v_tablenormalization',$rij); ?>
                     
@@ -242,6 +243,7 @@
         </div>
     </div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
@@ -251,6 +253,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
         integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
+
 </body>
 
 
